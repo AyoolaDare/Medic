27811dc5db7d999f5fc97400a45b7420cb165f0d
Ayoola <40026791+AyoolaDare@users.noreply.github.com>
2024-12-06 22:04:23 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    AWS_REGION = os.environ.get('AWS_REGION')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    USERS_TABLE_NAME = os.environ.get('USERS_TABLE_NAME')
+    PATIENTS_TABLE_NAME = os.environ.get('PATIENTS_TABLE_NAME')
+    ACTIVITIES_TABLE_NAME = os.environ.get('ACTIVITIES_TABLE_NAME')
+
